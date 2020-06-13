75ff8e9505408cba75e47a0cb142a1ac852a99e1
menisEric <66187218+menisEric@users.noreply.github.com>
2020-06-13 01:39:00 -0300

    ButterKnife in Activity
    Implemento de la anotacione @BindView

--- app/src/main/java/com/ericmenis/map_activity/MainActivity.java
+++ app/src/main/java/com/ericmenis/map_activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ericmenis.map_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,23 +8,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton btnSearch;
-
-    private EditText editTextLAT, editTextLON;
-    private TextView textViewLAT, textViewLON;
-
-    private Button buttonAceptar;
+   //Boton principal
+    @BindView(R.id.buttonConfirm)
+    ImageButton btnSearch;
+    //Boton buscar
+    @BindView(R.id.buttonToSecondActivity)
+    Button buttonAceptar;
+    //EditText LATITUD y LONGITUD
+    @BindView(R.id.editTextLAT)
+    EditText editTextLAT;
+    @BindView(R.id.editTextLON)
+    EditText editTextLON;
+    //Titulo LATITUD y LONGITUD
+    @BindView(R.id.titleLAT)
+    TextView textViewLAT;
+    @BindView(R.id.titleLON)
+    TextView textViewLON;
 
     private Double latitud, longitud;
     private String lat, lon;
+    
+    @OnClick(R.id.buttonConfirm)
+    void onClicked(){
+        textViewLAT.setVisibility(View.VISIBLE);
+        textViewLON.setVisibility(View.VISIBLE);
+        editTextLAT.setVisibility(View.VISIBLE);
+        editTextLON.setVisibility(View.VISIBLE);
+        buttonAceptar.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getSupportActionBar().setLogo(R.mipmap.ic_app);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        btnSearch = (ImageButton) findViewById(R.id.buttonConfirm);
-
-        buttonAceptar = (Button) findViewById(R.id.buttonToSecondActivity);
-
-        editTextLAT = (EditText) findViewById(R.id.editTextLAT);
-        editTextLON = (EditText) findViewById(R.id.editTextLON);
-
-        textViewLAT = (TextView) findViewById(R.id.titleLAT);
-        textViewLON = (TextView) findViewById(R.id.titleLON);
-
-        btnSearch.setOnClickListener(this);
 
         buttonAceptar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,16 +78,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        textViewLAT.setVisibility(View.VISIBLE);
-        textViewLON.setVisibility(View.VISIBLE);
-        editTextLAT.setVisibility(View.VISIBLE);
-        editTextLON.setVisibility(View.VISIBLE);
-        buttonAceptar.setVisibility(View.VISIBLE);
     }
 }
 
